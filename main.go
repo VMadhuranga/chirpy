8b4ad5daf8d860597aa6408f3b7f21a033d5ec56
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-07-25 18:25:15 +0530

    Add handler for /healthz path and update file server paths

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	serveMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	serveMux.Handle("assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
+
+	// check server readiness
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
