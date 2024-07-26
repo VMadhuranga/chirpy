642518358962e710a65e21dbcfeafb9cf67ed0e7
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-07-26 10:46:46 +0530

    Update route paths and http methods

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg apiConfig) getMiddlewareMetrics() int {
+	return cfg.fileserverHits
+}
+
+func (cfg *apiConfig) resetMiddlewareMetrics() {
+	cfg.fileserverHits = 0
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -8,15 +30,30 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
+	cfg := apiConfig{}
 
-	serveMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serveMux.Handle("assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
+	serveMux.Handle("GET /app/*", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.Handle("GET /assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
 
 	// check server readiness
-	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	serveMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+	})
+
+	serveMux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		metric := cfg.getMiddlewareMetrics()
+		w.Write([]byte("Hits: " + strconv.Itoa(metric)))
+	})
+
+	serveMux.HandleFunc("GET /api/reset", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		cfg.resetMiddlewareMetrics()
+		w.Write([]byte("resettled"))
 	})
 
 	err := server.ListenAndServe()
