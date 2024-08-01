c7794ebc12ec9cf00617b7d0d813450bf4e87ac1
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-08-01 11:21:48 +0530

    Update user struct

--- database/database.go
+++ database/database.go
@@ -18,6 +18,7 @@ type user struct {
 	Id       int    `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password,omitempty"`
+	Token    string `json:"token,omitempty"`
 }
 
 type databaseStructure struct {
