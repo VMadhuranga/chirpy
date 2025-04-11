00865228f64f0c63e1a2d737e93bea07750b849f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2025-04-11 09:09:40 +0530

    Complete CH2: Routing

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v\n", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v\n", cfg.fileserverHits.Load())))
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -11,15 +37,18 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
+	apiConfig := &apiConfig{}
 
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serveMux.Handle("assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
 
-	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	serveMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
 	})
+	serveMux.HandleFunc("GET /metrics", apiConfig.getMetrics)
+	serveMux.HandleFunc("POST /reset", apiConfig.resetMetrics)
 
 	err := server.ListenAndServe()
 	if err != nil {
