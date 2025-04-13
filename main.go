ac729d0a4acb3c549840b5acb07db368e96bc0d7
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2025-04-13 15:22:52 +0530

    Complete CH4: JSON

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -37,6 +39,8 @@ func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.SetFlags(log.Lshortfile)
+
 	serveMux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
@@ -55,9 +59,67 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
 	})
+	serveMux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type payload struct {
+			Body string `json:"body"`
+		}
+
+		pld := payload{}
+		err := json.NewDecoder(r.Body).Decode(&pld)
+		if err != nil {
+			log.Printf("error decoding payload: %s\n", err)
+			respondWithError(w, http.StatusBadRequest, "error decoding payload")
+			return
+		}
+		defer r.Body.Close()
+
+		if len(pld.Body) > 140 {
+			log.Printf("payload body is too long\n")
+			respondWithError(w, http.StatusBadRequest, "payload body is too long")
+			return
+		}
+
+		type successResponse struct {
+			CleanedBody string `json:"cleaned_body"`
+		}
+		respondWithJSON(w, http.StatusOK, successResponse{
+			CleanedBody: removeProfane(pld.Body),
+		})
+	})
 
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	resData, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("error encoding payload: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(resData)
+}
+
+func removeProfane(input string) string {
+	cleanedInput := strings.Split(input, " ")
+
+	for i, v := range cleanedInput {
+		switch strings.ToLower(v) {
+		case "kerfuffle", "sharbert", "fornax":
+			cleanedInput[i] = "****"
+		}
+
+	}
+
+	return strings.Join(cleanedInput, " ")
 }
